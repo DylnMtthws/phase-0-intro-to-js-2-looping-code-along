9c97c9b1117f542d40f2de2a5ee07c305979bae2
dylanmatthews <djmatthews93@gmail.com>
2023-08-22 09:10:24 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const event = "surprise";
+  const messages = writeCards(names, event);
+  console.log(messages);
+  
+
+
+
+  
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  
